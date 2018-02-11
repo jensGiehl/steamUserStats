f07d01200b6a4061c883d2edf5af255a1e755fc3
Jens Giehl <mobil.jgiehl@gmail.com>
2018-02-11 20:27:42 +0100

    Change URL and comments

--- loadUserStats.sh
+++ loadUserStats.sh
@@ -9,11 +9,10 @@ cat myGames.json | grep "appid" | cut -d':' -f2 | sed -e 's/ //g' | sed -e 's/,/
 # Remove Gamelist
 rm myGames.json
 
-# Load Achievments for every App
+# Load UserStats for every App
 while read appId; do
-  logger "Load Achievments for AppId $appId"
-  #wget --no-http-keep-alive --output-document="$appId.json" -q "http://api.steampowered.com/ISteamUserStats/GetPlayerAchievements/v0001/?appid=$appId&key=$STEAM_KEY&steamid=$STEAM_ID"
-  ./wgetRetry.sh "http://api.steampowered.com/ISteamUserStats/GetPlayerAchievements/v0001/?appid=$appId&key=$STEAM_KEY&steamid=$STEAM_ID" "$appId.json"
+  logger "Load UserStats for AppId $appId"
+  ./wgetRetry.sh "http://api.steampowered.com/ISteamUserStats/GetUserStatsForGame/v0002/?appid=$appId&key=$STEAM_KEY&steamid=$STEAM_ID" "$appId.json"
   
 # Check if file is empty (typical when the app has no stats)
   fileSize=`wc -c < "$appId.json"`
